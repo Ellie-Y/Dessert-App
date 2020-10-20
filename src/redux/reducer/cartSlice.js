15ea435f4646b015243102ac0d28b4c297d72c34
Ellie <ellie.ye22@gmail.com>
2020-10-20 08:52:01 +0100

    ID bug fixed

--- src/redux/reducer/cartSlice.js
+++ src/redux/reducer/cartSlice.js
@@ -6,14 +6,14 @@ const cartSlice = createSlice({  // generated type {type: "shoppingCart/add"}
   reducers: {
     add: (state, action) => {
       const newItem = action.payload;
-      const hasItem = state.some(i => i.item.id === newItem.item.id);  //TODO id 有时候判断错误
-      if (!hasItem) {  // 没有就直接添加
+      const itemIndex = state.findIndex(i => i.item.id === newItem.item.id);
+      if (itemIndex === -1) {  // 没有就直接添加
         return [ ...state, newItem];
       }
-      else if (hasItem) {  // 有物品，在旧物品的基础上添加上新的数量，插入新对象并把旧物品删除
-        const changedItem = state.map(i => i.item.id === newItem.item.id ? {item: newItem.item, count: newItem.count + i.count} : i);
-        const oldItemIndex = state.findIndex(i => i.item.id === newItem.item.id);
-        state.splice(oldItemIndex, 1, changedItem[0]);
+      else {  // 有物品，在旧物品的基础上添加上新的数量，插入新对象并把旧物品删除
+        let changedItem = null;
+        state.forEach(i => i.item.id === newItem.item.id ? changedItem = {item: newItem.item, count: newItem.count + i.count} : i);
+        state.splice(itemIndex, 1, changedItem);
         return state;
       }
     },
